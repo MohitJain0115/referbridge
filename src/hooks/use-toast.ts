bafd4f4b4713cb025ccece87c5ccd37cb87ad69b
Mohit Jain <mohitjain3579@gmail.com>
2025-07-11 07:51:24 +0000

    still taking more than 2 seconds

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -9,7 +9,7 @@ import type {
 } from "@/components/ui/toast"
 
 const TOAST_LIMIT = 1
-const TOAST_REMOVE_DELAY = 2000
+const TOAST_REMOVE_DELAY = 500
 
 type ToasterToast = ToastProps & {
   id: string
@@ -156,6 +156,7 @@ function toast({ ...props }: Toast) {
     type: "ADD_TOAST",
     toast: {
       ...props,
+      duration: props.duration || 2000,
       id,
       open: true,
       onOpenChange: (open) => {
